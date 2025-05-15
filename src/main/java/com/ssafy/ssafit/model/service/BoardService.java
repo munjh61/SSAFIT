26c3defdcc0209c973f7a04224822b5281e45770
yeonwju <yeonwju@gmail.com>
2025-05-15 17:07:41 +0900

    보드 컨트롤러 남음

--- src/main/java/com/ssafy/ssafit/model/service/BoardService.java
+++ src/main/java/com/ssafy/ssafit/model/service/BoardService.java
@@ -6,13 +6,13 @@ import java.util.List;
 
 public interface BoardService {
     //board 검색
-    public List<Board> select(Board board);
+    List<Board> select(Board board);
     //board 상세 조회
-    public Board selectDetail(String boardId);
+    Board selectDetail(String boardId);
     //board 등록
-    public void insert(Board board);
+    void insert(Board board);
     //board 수정
-    public void update(String boardId);
+    void update(String boardId);
     //board 삭제
-    public boolean delete(String boardId);
+    boolean delete(String boardId);
 }
