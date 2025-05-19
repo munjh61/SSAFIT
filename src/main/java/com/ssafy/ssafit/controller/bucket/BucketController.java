0980e77746b203674ab164c3e92ff9243a0dc0bb
yeonwju <yeonwju@gmail.com>
2025-05-19 17:32:37 +0900

    commit controller 전까지

--- src/main/java/com/ssafy/ssafit/controller/bucket/BucketController.java
+++ src/main/java/com/ssafy/ssafit/controller/bucket/BucketController.java
@@ -1,4 +1,17 @@
 package com.ssafy.ssafit.controller.bucket;
 
+import com.ssafy.ssafit.model.service.BucketService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("api/bucket")
+@RequiredArgsConstructor
 public class BucketController {
+     private final BucketService bucketService;
+
+
 }
