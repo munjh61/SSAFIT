101bea1792cc8bbee74f76152d82ebb4382033c8
munjh61 <junho9661@gmail.com>
2025-05-26 16:43:51 +0900

    팔로우 95% 완료

--- back/src/main/java/com/ssafy/ssafit/model/dto/Follow.java
+++ back/src/main/java/com/ssafy/ssafit/model/dto/Follow.java
@@ -10,8 +10,12 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Follow {
-    private int followID;
+    private int followId;
     private String followerId;
     private String followingId;
     private LocalDateTime createdAt;
+    // 이 아래 부분은 table에 없음. mapper에서 불러올 때 dto 새로 만들기 귀찮아서 여기에 씀
+    private String followerName;
+    private String followingName;
+
 }
--- front/src/stores/follow.js
+++ front/src/stores/follow.js
@@ -1,32 +1,68 @@
 import { computed, ref } from "vue";
 import { defineStore } from "pinia";
 import axios from "axios";
-
 const serverUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const useFollowStore = defineStore("follow", () => {
   const mutual = ref([]);
   const follower = ref([]);
-  const followerCnt = computed(()=>{
-    return follower.value
-  })
+  const followerCnt = computed(() => {
+    return mutual.value.length + follower.value.length;
+  });
   const following = ref([]);
-  const followingCnt = computed(()=>{
-    return following.value
+  const followingCnt = computed(() => {
+    return mutual.value.length + following.value.length;
   });
 
   const getFollowData = async (userId) => {
     return axios({
       url: `${serverUrl}/api/public/follow/${userId}`,
     }).then((res) => {
-      console.log(res.data.mutual); // 서로 팔로우
-      console.log(res.data.onlyYou); // 상대만 userId를 팔로우
-      console.log(res.data.onlyMe); // userId만 상대를 팔로우
-        follower.value = [...res.data.mutual, ...res.data.onlyYou]
-        following.value = [...res.data.mutual, ...res.data.onlyMe]
-
+      mutual.value = [...res.data.mutual]; // 서로 팔로우
+      follower.value = [...res.data.onlyYou]; // 상대만 userId를 팔로우
+      following.value = [...res.data.onlyMe]; // userId만 상대를 팔로우
     });
   };
 
-  return { getFollowData };
+  const addFollow = (userId) =>{
+    if(sessionStorage.getItem('ssafit-login-token')==null){
+      alert("로그인 후 이용 가능합니다.")
+    }
+    return axios({
+      url:`${serverUrl}/api/follow/${userId}`,
+      method:"POST",
+      headers:{
+        Authorization: `Bearer ${sessionStorage.getItem('ssafit-login-token')}`
+      }
+    })
+    .then((res)=>{
+      //console.log(res.data)
+      return true
+    })
+    .catch((err)=>{
+      //console.log(err.response.data)
+      return false
+    })
+  }
+
+  const deleteFollow = (userId) =>{
+    return axios({
+      url:`${serverUrl}/api/follow/${userId}`,
+      method:"DELETE",
+      headers:{
+        Authorization: `Bearer ${sessionStorage.getItem('ssafit-login-token')}`
+      }
+    })
+    .then((res)=>{
+      //console.log(res.data)
+      return true
+    })
+    .catch((err)=>{
+      //console.log(err.response.data)
+      return false
+    })
+  }
+
+
+  return { mutual, follower, followerCnt, following, followingCnt, getFollowData, addFollow, deleteFollow };
 });
